7bee56f12001fb79d03d191428d879ae887bc5c3
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2026-03-06 10:47:03 -0300

    .

--- Módulo6 - Django/Projetos/Projeto1/pessoas/views.py
+++ Módulo6 - Django/Projetos/Projeto1/pessoas/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from datetime import date
+from .models import Pessoa
+#from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Ola Django!")
+    # return HttpResponse("Olá Django!")
+    p = Pessoa.objects.get(id=1)
+    contexto = {"pessoas": p}
+    return render(request, 'home.html', contexto)
